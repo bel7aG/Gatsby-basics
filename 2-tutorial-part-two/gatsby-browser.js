5023c3aa4d6cc0dd09175804451f2fb775fb44c6
bel7aG <belhassen.gharsallah@isetrades.com>
2019-01-10 16:26:34 +0100

    undrestand how gatsby do style things

---
+++ 2-tutorial-part-two/gatsby-browser.js
@@ -0,0 +1,7 @@
+// is one of a handful of special files that Gatsby looks for,
+// and uses (if they exist). Here, the naming of the file is important.
+// If you do want to explore more now, check out the docs 
+// https://www.gatsbyjs.org/docs/browser-apis/
+
+import './src/styles/global.css';
+
