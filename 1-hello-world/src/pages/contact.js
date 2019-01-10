310f3d618b5a5cd4ba6ed167a766386efde3de52
bel7aG <belhassen.gharsallah@isetrades.com>
2019-01-10 15:57:16 +0100

    gatsby i will complete you

--- 1-hello-world/src/pages/contact.js
+++ 1-hello-world/src/pages/contact.js
@@ -1,11 +1,16 @@
-import React from "react"
+import React from 'react'
 import Header from '../components/Header'
 import { Link } from 'gatsby'
 
+const linkStyle = {
+  color: '#2d76f2',
+  textDecoration: 'none'
+}
+
 const Contact = () => (
   <div className="content">
-    <Header content='This is contact page' />
-    <Link to="/">Home</Link>
+    <Header content='This is contact page'/>
+    <Link to="/">Homeeeeeeees</Link>
   </div>
 )
 
